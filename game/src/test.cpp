0f8513351a8846925e687a0750a440f48bc6cc33
Álex Mesquita <alex.mesquita06@hotmail.com>
2015-05-07 14:19:12 -0300

    left movimentation

--- game/src/test.cpp
+++ game/src/test.cpp
@@ -2,6 +2,8 @@
 
 #include <stdlib.h>
 
+#define VELOCITY 5
+
 Uint32 getpixel(SDL_Surface *surface, int x, int y);
 void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
 
@@ -31,58 +33,125 @@ int main()
     SDL_Surface *hall = SDL_DisplayFormat(img);
     SDL_FreeSurface(img);
 
-    SDL_Rect center { 250, 150, 300, 300 }; 
+    /* 2 paredes */
+    // SDL_Rect center { 250, 150, 300, 300 }; 
+    // map_center(screen, door, center);
+
+    // SDL_Rect left { 250, 150, 250, 300 };
+    // map_left(screen, door, left, 0.6);
+
+    // SDL_Rect right { 550, 150, 250, 300 };
+    // map_right(screen, door, right, 0.6);
+
+    // SDL_Rect up { 0, 0, 800, 150 };
+    // map_up(screen, hall, up, 1.7);
+
+    SDL_Rect center { 330, 230, 140, 140 }; 
     map_center(screen, door, center);
 
-    SDL_Rect left { 250, 150, 250, 300 };
-    map_left(screen, door, left, 0.6);
-
-    SDL_Rect right { 550, 150, 250, 300 };
-    map_right(screen, door, right, 0.6);
-
-    SDL_Rect up { 0, 0, 800, 150 };
-    map_up(screen, hall, up, 1.7);
-
-/*    SDL_Rect center { 300, 200, 200, 200 }; 
-    map_center(screen, door, center);
-
-    SDL_Rect left { 300, 200, 150, 200 };
+    /* 3 paredes com o mesmo tamanho */
+    SDL_Rect left { 330, 230, 110, 140 };
     map_left(screen, hall, left, 0.5);
 
-    left.x = 150;
-    left.y = 125;
-    left.h = 350;
-    map_left(screen, door, left, 0.5);
+    left.x = 220;
+    left.y = 175;
+    left.w = 110;
+    left.h = 250;
+    map_left(screen, hall, left, 0.5);
 
-    SDL_Rect right { 500, 200, 150, 200 };
+    left.x = 110;
+    left.y = 120;
+    left.w = 110;
+    left.h = 360;
+    map_left(screen, hall, left, 0.5);
+
+    SDL_Rect right { 470, 230, 110, 140 };
     map_right(screen, hall, right, 0.5);
 
-    right.x = 650;
-    right.y = 125;
-    right.h = 350;
-    map_right(screen, hall, right, 0.5); */
+    right.x = 580;
+    right.y = 175;
+    right.w = 110;
+    right.h = 250;
+    map_right(screen, hall, right, 0.5);
+
+    right.x = 690;
+    right.y = 120;
+    right.w = 110;
+    right.h = 360;
+    map_right(screen, hall, right, 0.5);
+
+    /* 3 parades da esquerda com o tamanho diferente */
+    // SDL_Rect left { 330, 230, 55, 140 };
+    // map_left(screen, hall, left, 0.5);
+
+    // left.x = 275;
+    // left.y = 200;
+    // left.w = 165;
+    // left.h = 200;
+    // map_left(screen, hall, left, 0.5);
+
+    // left.x = 110;
+    // left.y = 120;
+    // left.w = 110;
+    // left.h = 360;
+    // map_left(screen, hall, left, 0.5);
+
+    // SDL_Rect right { 500, 200, 150, 200 };
+    // map_right(screen, hall, right, 0.5);
+
+    // right.x = 650;
+    // right.y = 125;
+    // right.h = 350;
+    // map_right(screen, hall, right, 0.5); 
 
 
     SDL_Flip(screen);
 
-    SDL_FreeSurface(door);
-    SDL_FreeSurface(hall);
-
     SDL_Event event;
+
+    int passo = 1;
 
     while (true)
     {
         while (SDL_PollEvent(&event))
         {
-            if (event.type == SDL_QUIT or event.type == SDL_KEYDOWN)
+            if (event.type == SDL_QUIT)
             {
                 return 0;
             }
+            else if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.sym == SDLK_UP)
+                {
+                    left.x = 330 - VELOCITY*passo;
+                    left.y = 230 - VELOCITY*passo*0.5;
+                    left.w = 110;
+                    left.h = 140 + VELOCITY*passo;
+                    map_left(screen, hall, left, 0.5);
 
-            SDL_Delay(1);
+                    left.x = 220 - VELOCITY*passo;
+                    left.y = 175 - VELOCITY*passo*0.5;
+                    left.w = 110;
+                    left.h = 250 + VELOCITY*passo;
+                    map_left(screen, hall, left, 0.5);
+
+                    left.x = 110 - VELOCITY*passo;
+                    left.y = 120 - VELOCITY*passo*0.5;
+                    left.w = 110;
+                    left.h = 360 + VELOCITY*passo;
+                    map_left(screen, hall, left, 0.5);
+
+                    passo++;
+                    SDL_Flip(screen);
+                }
+            }
+            SDL_Delay(10);
         }
+        SDL_Delay(10);
     }
 
+    SDL_FreeSurface(door);
+    SDL_FreeSurface(hall);
     return 0;
 }
 
@@ -175,7 +244,7 @@ void map_left(SDL_Surface *screen, SDL_Surface *img, const SDL_Rect& dest,
     double top_y = dest.y;
     double bot_y = dest.y + dest.h - 1;
 
-    for (int i = dest.x; i > dest.x - dest.w; --i)
+    for (int i = dest.x; i > dest.x - dest.w && i > 0; --i)
     {
         for (int j = top_y; j <= bot_y; ++j)
         {
