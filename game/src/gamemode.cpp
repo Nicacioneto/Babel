c45db173311de9dcf9e041adbe06524c0f135042
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2015-05-15 19:44:15 -0300

    fixing game modes #25

--- game/src/gamemode.cpp
+++ game/src/gamemode.cpp
@@ -3,9 +3,11 @@
 #define X_BUTTON 25
 #define Y_BUTTON 28
 #define W_BUTTON 305
+#define H_BUTTON 716
 
 GameMode::GameMode(const string& next, const string&)
-    : Level("", next), m_babelmode(nullptr), m_colonymode(nullptr), m_planetmode(nullptr)
+    : Level("", next), m_babelmode(nullptr), m_colonymode(nullptr), m_planetmode(nullptr),
+        m_babel(nullptr), m_colony(nullptr), m_planet(nullptr)
 {
     env = Environment::get_instance();
 
@@ -13,9 +15,12 @@ GameMode::GameMode(const string& next, const string&)
     m_colonymode = env->resources_manager->get_texture("res/images/modes/colonymode.png");
     m_planetmode = env->resources_manager->get_texture("res/images/modes/planetmode.png");
 
-    m_babel = new Button(this, "babel", "res/images/modes/button.png");
-    m_colony = new Button(this, "colony", "res/images/modes/button.png");
-    m_planet = new Button(this, "planet", "res/images/modes/button.png");
+    m_babel = new Button(this, "babel", "res/images/modes/button.png", X_BUTTON,
+        Y_BUTTON, W_BUTTON, H_BUTTON);
+    m_colony = new Button(this, "colony", "res/images/modes/button.png", X_BUTTON,
+        Y_BUTTON, W_BUTTON, H_BUTTON);
+    m_planet = new Button(this, "planet", "res/images/modes/button.png", X_BUTTON,
+        Y_BUTTON, W_BUTTON, H_BUTTON);
 
     m_babel->add_observer(this);
     m_colony->add_observer(this);
@@ -39,13 +44,13 @@ GameMode::update_self(unsigned long)
 void
 GameMode::draw_self()
 {
+    env->canvas->clear();
+
     double scale = env->canvas->scale();
 
-    env->canvas->clear();
     env->canvas->draw(m_babelmode.get(), X_BUTTON * scale, Y_BUTTON * scale);
     env->canvas->draw(m_colonymode.get(), (2*X_BUTTON + W_BUTTON) * scale, Y_BUTTON * scale);
     env->canvas->draw(m_planetmode.get(), (3*X_BUTTON + 2*W_BUTTON) * scale, Y_BUTTON * scale);
-
 }
 
 bool
@@ -60,18 +65,18 @@ GameMode::on_message(Object *sender, MessageID id, Parameters)
 
     if (button->id() == "babel")
     {
-        // m_next = "gameover";
-        // m_done = true;
+        m_next = "gameover";
+        m_done = true;
     }
     else if (button->id() == "colony")
     {
-        // m_next = "gameover";
-        // m_done = true;
+        m_next = "gameover";
+        m_done = true;
     }
     else if (button->id() == "planet")
     {
-        // m_next = "gameover";
-        // m_done = true;
+        m_next = "gameover";
+        m_done = true;
     }
 
     return true;
