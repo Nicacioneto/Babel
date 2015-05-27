7eed815a162e3af9d486c5783ec9e455207e3da6
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2015-05-27 15:26:02 -0300

    updated colony

--- game/include/colony.h
+++ game/include/colony.h
@@ -13,7 +13,11 @@ public:
     Colony(const string& next = "");
     
 private:
+    shared_ptr<Texture> m_colony_screen;
     shared_ptr<Texture> m_colony;
+    shared_ptr<Texture> m_center_bracket;
+    shared_ptr<Texture> m_tower;
+    shared_ptr<Texture> m_planet;
 
     void draw_self();
 };
