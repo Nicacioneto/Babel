0ba3b3a791916227abb85553bc1f8e94b294ec57
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2015-06-03 19:09:20 -0300

    vera++ adjustments

--- game/src/character.cpp
+++ game/src/character.cpp
@@ -7,30 +7,30 @@ Character::Character(Object *parent, ObjectID id, const string& texture,
     : Object(parent, id, x, y, w, h), m_texture(nullptr), m_lvl(1), m_life(100),
     m_attack(10), m_defense(5)
 {
-	Environment *env = Environment::get_instance();
-	m_texture = env->resources_manager->get_texture(texture);
+    Environment *env = Environment::get_instance();
+    m_texture = env->resources_manager->get_texture(texture);
 }
 
 int
 Character::lvl()
 {
-	return m_lvl;
+    return m_lvl;
 }
 
 double
 Character::life()
 {
-	return m_life;
+    return m_life;
 }
 
 double
 Character::attack()
 {
-	return m_attack;
+    return m_attack;
 }
 
 double
 Character::defense()
 {
-	return m_defense;
+    return m_defense;
 }
