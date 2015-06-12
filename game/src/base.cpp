b217044242255a2b5bd3a7c0d565989a283e3014
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2015-06-11 22:05:40 -0300

    adapting base

--- game/src/base.cpp
+++ game/src/base.cpp
@@ -2,6 +2,9 @@
 #include "colony.h"
 #include <core/font.h>
 
+#define W 1024.0
+#define H 768.0
+
 Base::Base(const string& next)
     : Level("base", next), m_scenario(nullptr)
 {
@@ -23,8 +26,7 @@ Base::draw_self()
     Environment *env = Environment::get_instance();
     env->canvas->clear();
 
-    double scale = 1;
-    env->canvas->draw(m_scenario.get(), 275 * scale, 173 * scale);
+    env->canvas->draw(m_scenario.get(), (275 / W) * env->canvas->w(), (173 / H) * env->canvas->h());
 }
 
 bool
@@ -54,35 +56,39 @@ Base::on_message(Object *sender, MessageID id, Parameters)
 void
 Base::create_buttons()
 {
-    double scale = 1;
+    Environment *env = Environment::get_instance();
     string path = "res/images/colony/";
 
+    const int x = (28 / W) * env->canvas->w();
+    const int w = (190 / W) * env->canvas->w();
+    const int h = (180/3 / H) * env->canvas->h();
+
     Button *button =  new Button(this, "barracks", path + "barracks_button.png",
-        28 * scale, 218 * scale, 190 * scale, 180/3* scale);
+        x, (218 / H) * env->canvas->h(), w, h);
     button->set_sprites(3);
 
     m_buttons[button->id()] = button;
 
     button = new Button(this, "facilities", path + "facilities_button.png",
-        28 * scale, 322 * scale, 190 * scale, 180/3* scale);
+        x, (322 / H) * env->canvas->h(), w, h);
     button->set_sprites(3);
 
     m_buttons[button->id()] = button;
 
     button = new Button(this, "hospital", path + "hospital_button.png",
-        28 * scale, 427 * scale, 190 * scale, 180/3* scale);
+        x, (427 / H) * env->canvas->h(), w, h);
     button->set_sprites(3);
 
     m_buttons[button->id()] = button;
 
     button = new Button(this, "workshop", path + "workshop_button.png",
-        28 * scale, 531 * scale, 190 * scale, 180/3* scale);
+        x, (531 / H) * env->canvas->h(), w, h);
     button->set_sprites(3);
 
     m_buttons[button->id()] = button;
 
     button = new Button(this, "central", path + "central_button.png",
-        28 * scale, 635 * scale, 190 * scale, 180/3* scale);
+        x, (635 / H) * env->canvas->h(), w, h);
     button->set_sprites(3);
 
     m_buttons[button->id()] = button;
