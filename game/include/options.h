de9ee4075c94b2d454937494cb764185d6cde63e
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-06-18 20:40:24 -0300

    Refactoring options

--- game/include/options.h
+++ game/include/options.h
@@ -5,27 +5,24 @@
 #include "file.h"
 #include <core/level.h>
 #include <memory>
+#include <map>
 #include <vector>
+
+using std::map;
+using std::vector;
 
 class Texture;
 
 class Options : public Level
 {
 public:
-    Options(const string& next = "menu",
-        const string& texture = "res/images/menu/init-screen.png");
+    Options(const string& next = "menu");
 
 private:
-    shared_ptr<Texture> m_texture;
-    shared_ptr<Texture> m_logo;
-    shared_ptr<Texture> m_soundvideo;
-    shared_ptr<Texture> m_volume;
-    shared_ptr<Texture> m_arrow;
-    Button *m_up_volume, *m_down_volume;
-    Button *m_up_resolution, *m_down_resolution;
-    Button *m_back;
+    map<string, shared_ptr<Texture>> m_textures;
+    map<string, Button *> m_buttons;
 
-    std::vector<int> m_resolutions = {800, 1024};
+    vector<int> m_resolutions = {800, 1024};
 
     void draw_self();
     void update_coordinates();
--- game/src/options.cpp
+++ game/src/options.cpp
@@ -9,18 +9,16 @@
 #define W 1024.0
 #define H 768.0
 
-Options::Options(const string& next, const string& texture)
-    : Level("options", next), m_texture(nullptr), m_logo(nullptr), m_soundvideo(nullptr),
-        m_volume(nullptr), m_arrow(nullptr), m_up_volume(nullptr), m_down_volume(nullptr),
-        m_up_resolution(nullptr), m_down_resolution(nullptr), m_back(nullptr)
+Options::Options(const string& next)
+    : Level("options", next)
 {
     Environment *env = Environment::get_instance();
 
-    m_texture = env->resources_manager->get_texture(texture);
-    m_logo = env->resources_manager->get_texture("res/images/menu/babel-logo.png");
-    m_soundvideo = env->resources_manager->get_texture("res/images/menu/sound-video.png");
-    m_volume = env->resources_manager->get_texture("res/images/menu/volume.png");
-    m_arrow = env->resources_manager->get_texture("res/images/menu/arrow.png");
+    m_textures["background"] = env->resources_manager->get_texture("res/images/menu/init-screen.png");
+    m_textures["logo"] = env->resources_manager->get_texture("res/images/menu/babel-logo.png");
+    m_textures["soundvideo"] = env->resources_manager->get_texture("res/images/menu/sound-video.png");
+    m_textures["volume"] = env->resources_manager->get_texture("res/images/menu/volume.png");
+    m_textures["arrow"] = env->resources_manager->get_texture("res/images/menu/arrow.png");
 
     shared_ptr<Font> font = env->resources_manager->get_font("res/fonts/exo-2/Exo2.0-Regular.otf");
     env->canvas->set_font(font);
@@ -31,32 +29,31 @@ Options::Options(const string& next, const string& texture)
     int w = (140/W) * env->canvas->w();
     int h = (60/H) * env->canvas->h();
 
-    m_back = new Button(this, "back", "res/images/menu/button.png", x, y, w, h);
-    m_back->set_text("Back");
+    Button *button = new Button(this, "back", "res/images/menu/button.png", x, y, w, h);
+    button->set_text("Back");
+    m_buttons[button->id()] = button;
 
-    m_up_volume = new Button(this, "up_volume", "",
+    button = new Button(this, "up_volume", "",
         300, (env->canvas->h() - 25)/2, 12, 12);
+    m_buttons[button->id()] = button;
 
-    m_down_volume = new Button(this, "down_volume", "",
+    button = new Button(this, "down_volume", "",
         300, env->canvas->h()/2, 12, 12);
+    m_buttons[button->id()] = button;
 
-    m_up_resolution = new Button(this, "up_resolution", "",
+    button = new Button(this, "up_resolution", "",
         env->canvas->w()/2 + 140, (env->canvas->h() - 25)/2, 12, 12);
+    m_buttons[button->id()] = button;
 
-    m_down_resolution = new Button(this, "down_resolution", "",
+    button = new Button(this, "down_resolution", "",
         env->canvas->w()/2 + 140, (env->canvas->h())/2, 12, 12);
+    m_buttons[button->id()] = button;
 
-    m_back->add_observer(this);
-    m_up_volume->add_observer(this);
-    m_down_volume->add_observer(this);
-    m_up_resolution->add_observer(this);
-    m_down_resolution->add_observer(this);
-
-    add_child(m_back);
-    add_child(m_up_volume);
-    add_child(m_down_volume);
-    add_child(m_up_resolution);
-    add_child(m_down_resolution);
+    for (auto b : m_buttons)
+    {
+        b.second->add_observer(this);
+        add_child(b.second);
+    }
 }
 
 void
@@ -68,31 +65,31 @@ Options::update_coordinates()
     int y = (H - 149)/H * env->canvas->h();
     int w = (140/W) * env->canvas->w();
     int h = (60/H) * env->canvas->h();
-    m_back->set_position(x, y);
-    m_back->set_dimensions(w, h);
-    m_back->set_text("Back");
+    m_buttons["back"]->set_position(x, y);
+    m_buttons["back"]->set_dimensions(w, h);
+    m_buttons["back"]->set_text("Back");
 
     x = (300)/W * env->canvas->w();
     y = (H - 25)/(H * 2) * env->canvas->h();
     w = (12/W) * env->canvas->w();
     h = (12/H) * env->canvas->h();
-    m_up_volume->set_position(x, y);
-    m_up_volume->set_dimensions(w, h);
+    m_buttons["up_volume"]->set_position(x, y);
+    m_buttons["up_volume"]->set_dimensions(w, h);
 
     y = env->canvas->h()/2;
-    m_down_volume->set_position(x, y);
-    m_down_volume->set_dimensions(w, h);
+    m_buttons["down_volume"]->set_position(x, y);
+    m_buttons["down_volume"]->set_dimensions(w, h);
 
     x = (W/2 + 140)/W * env->canvas->w();
     y = (H - 25)/(H * 2) * env->canvas->h();
     w = (12/W) * env->canvas->w();
     h = (12/H) * env->canvas->h();
-    m_up_resolution->set_position(x, y);
-    m_up_resolution->set_dimensions(w, h);
+    m_buttons["up_resolution"]->set_position(x, y);
+    m_buttons["up_resolution"]->set_dimensions(w, h);
 
     y = (env->canvas->h())/2;
-    m_down_resolution->set_position(x, y);
-    m_down_resolution->set_dimensions(w, h);
+    m_buttons["down_resolution"]->set_position(x, y);
+    m_buttons["down_resolution"]->set_dimensions(w, h);
 }
 
 void
@@ -105,23 +102,23 @@ Options::draw_self()
     shared_ptr<Settings> settings = env->resources_manager->get_settings(env->m_settings_path);
 
     env->canvas->clear();
-    env->canvas->draw(m_texture.get());
+    env->canvas->draw(m_textures["background"].get());
 
-    int x = (env->canvas->w() - m_logo->w())/2;
+    int x = (env->canvas->w() - m_textures["logo"]->w())/2;
     int y = 25/H * env->canvas->h();
     int w, h;
-    env->canvas->draw(m_logo.get(), x, y);
+    env->canvas->draw(m_textures["logo"].get(), x, y);
 
     x = 189/W * env->canvas->w();
     y = 321/H * env->canvas->h();
-    env->canvas->draw(m_soundvideo.get(), x, y);
+    env->canvas->draw(m_textures["soundvideo"].get(), x, y);
 
     x = 300/W * env->canvas->w();
     y = (H - 20)/(H * 2) * env->canvas->h();
-    env->canvas->draw(m_arrow.get(), x, y);
+    env->canvas->draw(m_textures["arrow"].get(), x, y);
     
     x = (W/2 + 140)/W * env->canvas->w();
-    env->canvas->draw(m_arrow.get(), x, y);
+    env->canvas->draw(m_textures["arrow"].get(), x, y);
 
     int i, volume = settings->read<int>("Game", "volume", 50)/10;
 
@@ -131,7 +128,7 @@ Options::draw_self()
         y = (H - 15)/(H*2) * env->canvas->h();
         h = 15/W * env->canvas->w();
         w = h;
-        env->canvas->draw(m_volume.get(), Rect(0, 15, 15, 15), x, y, h, w);
+        env->canvas->draw(m_textures["volume"].get(), Rect(0, 15, 15, 15), x, y, h, w);
     }
     for (int j = i; j < i + volume*17; j+=17)
     {
@@ -139,7 +136,7 @@ Options::draw_self()
         y = (H - 15)/(H*2) * env->canvas->h();
         h = 15/W * env->canvas->w();
         w = h;
-        env->canvas->draw(m_volume.get(), Rect(0, 0, 15, 15), x, y, w, h);
+        env->canvas->draw(m_textures["volume"].get(), Rect(0, 0, 15, 15), x, y, w, h);
     }
 
     x = 465/W * env->canvas->w();
