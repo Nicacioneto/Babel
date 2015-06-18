ffcae7f3da25b18cfa156e22517a5bfa0b9b0bdf
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2015-06-17 21:45:44 -0300

    init refactor in barracks

--- game/include/barracks.h
+++ game/include/barracks.h
@@ -9,6 +9,7 @@
 using std::map;
 
 class Texture;
+class Character;
 
 class Barracks : public Level
 {
@@ -17,11 +18,30 @@ public:
 
 private:
     map<ObjectID, Button*> m_buttons;
+    map<ObjectID, Character*> m_characters;
     shared_ptr<Settings> m_settings;
+    shared_ptr<Texture> m_big_card;
+    shared_ptr<Texture> m_bracket;
+    shared_ptr<Texture> m_bracket_m;
+    shared_ptr<Texture> m_bracket_p;
+    shared_ptr<Texture> m_bracket_t;
+    shared_ptr<Texture> m_attributes;
+    shared_ptr<Texture> m_stats;
+    shared_ptr<Texture> m_levelup;
+    shared_ptr<Texture> m_military;
+    shared_ptr<Texture> m_psionic;
+    shared_ptr<Texture> m_tech;
+    shared_ptr<Texture> m_equip;
+    shared_ptr<Texture> m_rifle;
+    shared_ptr<Texture> m_armor;
+    shared_ptr<Texture> m_shield;
+    shared_ptr<Texture> m_equip_change;
+
     int m_character;
 
-    bool on_message(Object *sender, MessageID id, Parameters p);
     void draw_self();
+    bool on_message(Object *sender, MessageID id, Parameters p);
+    void load_characters();
 };
 
 #endif
--- game/src/barracks.cpp
+++ game/src/barracks.cpp
@@ -1,8 +1,11 @@
-#include "colony.h"
 #include "barracks.h"
+#include "character.h"
+#include "colony.h"
 #include <core/font.h>
 #include <core/rect.h>
 #include <core/settings.h>
+#include <iostream>
+using namespace std;
 
 #define W 1024.0
 #define H 768.0
@@ -10,7 +13,11 @@
 using std::to_string;
 
 Barracks::Barracks(const string& next)
-    : Level("barracks", next), m_character(0)
+    : Level("barracks", next), m_settings(nullptr), m_big_card(nullptr),
+    m_bracket(nullptr), m_bracket_m(nullptr), m_bracket_p(nullptr), m_bracket_t(nullptr),
+    m_attributes(nullptr), m_stats(nullptr), m_levelup(nullptr),
+    m_military(nullptr), m_psionic(nullptr), m_tech(nullptr), m_equip(nullptr),
+    m_rifle(nullptr), m_armor(nullptr), m_shield(nullptr), m_equip_change(nullptr), m_character(0)
 {
     Environment *env = Environment::get_instance();
     string path = "res/images/colony/barracks/";
@@ -31,6 +38,25 @@ Barracks::Barracks(const string& next)
         add_child(b.second);
     }
 
+    m_big_card = env->resources_manager->get_texture(path + "card_big.png");
+    m_bracket = env->resources_manager->get_texture(path + "bracket.png");
+    m_bracket_m = env->resources_manager->get_texture(path + "bracket-m.png");
+    m_bracket_p = env->resources_manager->get_texture(path + "bracket-p.png");
+    m_bracket_t = env->resources_manager->get_texture(path + "bracket-t.png");
+    m_attributes = env->resources_manager->get_texture(path + "attributes.png");
+    m_stats = env->resources_manager->get_texture(path + "stats.png");
+    m_levelup = env->resources_manager->get_texture(path + "levelup.png");
+    m_military = env->resources_manager->get_texture(path + "military.png");
+    m_psionic = env->resources_manager->get_texture(path + "psionic.png");
+    m_tech = env->resources_manager->get_texture(path + "tech.png");
+    m_equip = env->resources_manager->get_texture(path + "equip_shelf.png");
+    m_rifle  = env->resources_manager->get_texture(path + "rifle.png");
+    m_armor  = env->resources_manager->get_texture(path + "armor.png");
+    m_shield  = env->resources_manager->get_texture(path + "shield.png");
+    m_equip_change = env->resources_manager->get_texture(path + "equip_change.png");
+
+    load_characters();
+
     m_settings = env->resources_manager->get_settings("res/datas/characters.sav");
 }
 
@@ -39,213 +65,162 @@ Barracks::draw_self()
 {
     Environment *env = Environment::get_instance();
     env->canvas->clear();
-    shared_ptr<Texture> texture;
-    string path = "res/images/colony/";
 
-    int x = (112/W) * env->canvas->w();
-    int y = (130/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "characters/psionic_big.png");
-    env->canvas->draw(texture.get(), x, y);
-    texture = env->resources_manager->get_texture(path + "characters/card_big.png");
-    env->canvas->draw(texture.get(), x, y);
+    env->canvas->draw(m_big_card.get(), (112 / W) * env->canvas->w(), (130 / H) * env->canvas->h());
+    env->canvas->draw(m_bracket.get(), (30 / W) * env->canvas->w(), (25 / H) * env->canvas->h());
+
+    int y = (470 / H) * env->canvas->h();
+    env->canvas->draw(m_bracket_m.get(), (112 / W) * env->canvas->w(), y);
+    env->canvas->draw(m_bracket_p.get(), (402 / W) * env->canvas->w(), y);
+    env->canvas->draw(m_bracket_t.get(), (690 / W) * env->canvas->w(), y);
+
+    y = (485 / H) * env->canvas->h();
+    env->canvas->draw(m_military.get(), (135 / W) * env->canvas->w(), y);
+    env->canvas->draw(m_psionic.get(), (420 / W) * env->canvas->w(), y);
+    env->canvas->draw(m_tech.get(), (710 / W) * env->canvas->w(), y);
+
+
+    y = (128 / H) * env->canvas->h();
+
+    env->canvas->draw(m_attributes.get(), (402 / W) * env->canvas->w(), y);
+    env->canvas->draw(m_stats.get(), (690 / W) * env->canvas->w(), y);
     
-    path += "barracks/";
-
-    x = (30/W) * env->canvas->w();
-    y = (25/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "bracket.png");
-    env->canvas->draw(texture.get(), x, y);
-
-    x = (112/W) * env->canvas->w();
-    y = (470/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "bracket-m.png");
-    env->canvas->draw(texture.get(), x, y);
-
-    x = (402/W) * env->canvas->w();
-    texture = env->resources_manager->get_texture(path + "bracket-m.png");
-    env->canvas->draw(texture.get(), x, y);
-
-    x = (690/W) * env->canvas->w();
-    texture = env->resources_manager->get_texture(path + "bracket-m.png");
-    env->canvas->draw(texture.get(), x, y);
-
-    x = (402/W) * env->canvas->w();
-    y = (128/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "attributes.png");
-    env->canvas->draw(texture.get(), x, y);
+    env->canvas->draw(m_levelup.get(), (402 / W) * env->canvas->w(), (322 / H) * env->canvas->h());
     
-    y = (322/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "levelup.png");
-    env->canvas->draw(texture.get(), x, y);
+    // x = (670/W) * env->canvas->w();
+    // y = (353/H) * env->canvas->h();
+    // texture = env->resources_manager->get_texture(path + "equip_change.png");
+    // env->canvas->draw(texture.get(), x, y);
 
-    x = (690/W) * env->canvas->w();
-    y = (128/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "stats.png");
-    env->canvas->draw(texture.get(), x, y);
+    // x = (130/W) * env->canvas->w();
+    // y = (530/H) * env->canvas->h();
 
-    y = (322/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "equip_shelf.png");
-    env->canvas->draw(texture.get(), x, y);
+    // for (int i = 1; i <= 5; ++i, x += 38)
+    // {
+    //     texture = env->resources_manager->get_texture(path + "Skill_M_" + to_string(i) + ".png");
+    //     env->canvas->draw(texture.get(), x, y);
+    // }
 
-    y = (360/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "rifle.png");
-    env->canvas->draw(texture.get(), x, y);
+    // for (int i = 0, x = 130; i < 5; ++i, x += 38)
+    // {
+    //     for (int j = 0, y = 567; j < 3; ++j, y += 37)
+    //     {
+    //         texture = env->resources_manager->get_texture(path + "Skill_M_Locked.png");
+    //         env->canvas->draw(texture.get(), (x/W) * env->canvas->w(), (y/H) * env->canvas->h());
+    //     }
+    // }
 
-    x = (772/W) * env->canvas->w();
-    texture = env->resources_manager->get_texture(path + "armor.png");
-    env->canvas->draw(texture.get(), x, y);
+    // x = (420/W) * env->canvas->w();
 
-    x = (855/W) * env->canvas->w();
-    texture = env->resources_manager->get_texture(path + "shield.png");
-    env->canvas->draw(texture.get(), x, y);
-    
-    x = (670/W) * env->canvas->w();
-    y = (353/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "equip_change.png");
-    env->canvas->draw(texture.get(), x, y);
+    // for (int i = 1; i <= 5; ++i, x += 38)
+    // {
+    //     texture = env->resources_manager->get_texture(path + "Skill_P_" + to_string(i) + ".png");
+    //     env->canvas->draw(texture.get(), x, y);
+    // }
 
-    x = (135/W) * env->canvas->w();
-    y = (485/H) * env->canvas->h();
-    texture = env->resources_manager->get_texture(path + "military.png");
-    env->canvas->draw(texture.get(), x, y);
+    // for (int i = 0, x = 420; i < 5; ++i, x += 38)
+    // {
+    //     for (int j = 0, y = 567; j < 3; ++j, y += 37)
+    //     {
+    //         texture = env->resources_manager->get_texture(path + "Skill_P_Locked.png");
+    //         env->canvas->draw(texture.get(), (x/W) * env->canvas->w(), (y/H) * env->canvas->h());
+    //     }
+    // }
 
-    x = (420/W) * env->canvas->w();
-    texture = env->resources_manager->get_texture(path + "psionic.png");
-    env->canvas->draw(texture.get(), x, y);
+    // x = (710/W) * env->canvas->w();
 
-    x = (710/W) * env->canvas->w();
-    texture = env->resources_manager->get_texture(path + "tech.png");
-    env->canvas->draw(texture.get(), x, y);
+    // for (int i = 1; i <= 5; ++i, x += 38)
+    // {
+    //     texture = env->resources_manager->get_texture(path + "Skill_T_" + to_string(i) + ".png");
+    //     env->canvas->draw(texture.get(), x, y);
+    // }
 
-    x = (130/W) * env->canvas->w();
-    y = (530/H) * env->canvas->h();
+    // for (int i = 0, x = 710; i < 5; ++i, x += 38)
+    // {
+    //     for (int j = 0, y = 567; j < 3; ++j, y += 37)
+    //     {
+    //         texture = env->resources_manager->get_texture(path + "Skill_T_Locked.png");
+    //         env->canvas->draw(texture.get(), (x/W) * env->canvas->w(), (y/H) * env->canvas->h());
+    //     }
+    // }
 
-    for (int i = 1; i <= 5; ++i, x += 38)
-    {
-        texture = env->resources_manager->get_texture(path + "Skill_M_" + to_string(i) + ".png");
-        env->canvas->draw(texture.get(), x, y);
-    }
+    // shared_ptr<Font> font = env->resources_manager->get_font("res/fonts/exo-2/Exo2.0-Regular.otf");
+    // env->canvas->set_font(font);
+    // font->set_size(32);
+    // Color color(170, 215, 190);
 
-    for (int i = 0, x = 130; i < 5; ++i, x += 38)
-    {
-        for (int j = 0, y = 567; j < 3; ++j, y += 37)
-        {
-            texture = env->resources_manager->get_texture(path + "Skill_M_Locked.png");
-            env->canvas->draw(texture.get(), (x/W) * env->canvas->w(), (y/H) * env->canvas->h());
-        }
-    }
+    // auto it = m_settings->sections().begin();
+    // for (int i = 0; i < m_character; ++it, ++i); // not very well with other ++ operators
 
-    x = (420/W) * env->canvas->w();
+    // string name = it->first;
+    // string levelup = it->second["levelup"];
+    // string military = it->second["military"];
+    // string psionic = it->second["psionic"];
+    // string tech = it->second["tech"];
+    // string might = it->second["might"];
+    // string mind = it->second["mind"];
+    // string perception = it->second["perception"];
+    // string agility = it->second["agility"];
+    // string might_attack = it->second["might_attack"];
+    // string mind_attack = it->second["mind_attack"];
+    // string cooldown = it->second["cooldown"];
+    // string defense = it->second["defense"];
+    // string might_armor = it->second["might_armor"];
+    // string mind_armor = it->second["mind_armor"];
+    // string critical = it->second["critical"];
 
-    for (int i = 1; i <= 5; ++i, x += 38)
-    {
-        texture = env->resources_manager->get_texture(path + "Skill_P_" + to_string(i) + ".png");
-        env->canvas->draw(texture.get(), x, y);
-    }
+    // if (levelup.back() == '\r')
+    // {
+    //     levelup.pop_back();
+    //     military.pop_back();
+    //     psionic.pop_back();
+    //     tech.pop_back();
+    //     might.pop_back();
+    //     mind.pop_back();
+    //     perception.pop_back();
+    //     agility.pop_back();
+    //     might_attack.pop_back();
+    //     mind_attack.pop_back();
+    //     cooldown.pop_back();
+    //     defense.pop_back();
+    //     might_armor.pop_back();
+    //     mind_armor.pop_back();
+    //     critical.pop_back();
+    // }
 
-    for (int i = 0, x = 420; i < 5; ++i, x += 38)
-    {
-        for (int j = 0, y = 567; j < 3; ++j, y += 37)
-        {
-            texture = env->resources_manager->get_texture(path + "Skill_P_Locked.png");
-            env->canvas->draw(texture.get(), (x/W) * env->canvas->w(), (y/H) * env->canvas->h());
-        }
-    }
+    // env->canvas->draw(name, (112/W) * env->canvas->w(), (90/H) * env->canvas->h(), color);
 
-    x = (710/W) * env->canvas->w();
+    // font->set_size(16);
+    // env->canvas->draw(name, (250/W) * env->canvas->w(), (129/H) * env->canvas->h(), color);
+    // font->set_size(18);
+    // env->canvas->draw(military, (295/W) * env->canvas->w(), (167/H) * env->canvas->h(), Color(208, 179, 43));
+    // env->canvas->draw(psionic, (295/W) * env->canvas->w(), (198/H) * env->canvas->h(), Color(166, 69, 151));
+    // env->canvas->draw(tech, (295/W) * env->canvas->w(), (229/H) * env->canvas->h(), Color(78, 191, 190));
 
-    for (int i = 1; i <= 5; ++i, x += 38)
-    {
-        texture = env->resources_manager->get_texture(path + "Skill_T_" + to_string(i) + ".png");
-        env->canvas->draw(texture.get(), x, y);
-    }
+    // font->set_size(22);
+    // env->canvas->draw(levelup, (500/W) * env->canvas->w(), (370/H) * env->canvas->h(), Color(62, 108, 236));
 
-    for (int i = 0, x = 710; i < 5; ++i, x += 38)
-    {
-        for (int j = 0, y = 567; j < 3; ++j, y += 37)
-        {
-            texture = env->resources_manager->get_texture(path + "Skill_T_Locked.png");
-            env->canvas->draw(texture.get(), (x/W) * env->canvas->w(), (y/H) * env->canvas->h());
-        }
-    }
+    // font->set_size(12);
+    // env->canvas->draw(might, (607/W) * env->canvas->w(), (165/H) * env->canvas->h(), color);
+    // env->canvas->draw(mind, (607/W) * env->canvas->w(), (200/H) * env->canvas->h(), color);
+    // env->canvas->draw(perception, (607/W) * env->canvas->w(), (235/H) * env->canvas->h(), color);
+    // env->canvas->draw(agility, (607/W) * env->canvas->w(), (270/H) * env->canvas->h(), color);
 
-    shared_ptr<Font> font = env->resources_manager->get_font("res/fonts/exo-2/Exo2.0-Regular.otf");
-    env->canvas->set_font(font);
-    font->set_size(32);
-    Color color(170, 215, 190);
+    // color = Color(82, 104, 93);
+    // env->canvas->draw(might_attack, (780/W) * env->canvas->w(), (165/H) * env->canvas->h(), color);
+    // env->canvas->draw(mind_attack, (780/W) * env->canvas->w(), (200/H) * env->canvas->h(), color);
+    // env->canvas->draw(cooldown, (780/W) * env->canvas->w(), (235/H) * env->canvas->h(), color);
+    // env->canvas->draw(defense, (780/W) * env->canvas->w(), (270/H) * env->canvas->h(), color);
 
-    auto it = m_settings->sections().begin();
-    for (int i = 0; i < m_character; ++it, ++i); // not very well with other ++ operators
+    // env->canvas->draw(might_armor, (900/W) * env->canvas->w(), (165/H) * env->canvas->h(), color);
+    // env->canvas->draw(mind_armor, (900/W) * env->canvas->w(), (200/H) * env->canvas->h(), color);
+    // env->canvas->draw(critical, (900/W) * env->canvas->w(), (235/H) * env->canvas->h(), color);
+    // env->canvas->draw(critical, (900/W) * env->canvas->w(), (270/H) * env->canvas->h(), color);
 
-    string name = it->first;
-    string levelup = it->second["levelup"];
-    string military = it->second["military"];
-    string psionic = it->second["psionic"];
-    string tech = it->second["tech"];
-    string might = it->second["might"];
-    string mind = it->second["mind"];
-    string perception = it->second["perception"];
-    string agility = it->second["agility"];
-    string might_attack = it->second["might_attack"];
-    string mind_attack = it->second["mind_attack"];
-    string cooldown = it->second["cooldown"];
-    string defense = it->second["defense"];
-    string might_armor = it->second["might_armor"];
-    string mind_armor = it->second["mind_armor"];
-    string critical = it->second["critical"];
-
-    if (levelup.back() == '\r')
-    {
-        levelup.pop_back();
-        military.pop_back();
-        psionic.pop_back();
-        tech.pop_back();
-        might.pop_back();
-        mind.pop_back();
-        perception.pop_back();
-        agility.pop_back();
-        might_attack.pop_back();
-        mind_attack.pop_back();
-        cooldown.pop_back();
-        defense.pop_back();
-        might_armor.pop_back();
-        mind_armor.pop_back();
-        critical.pop_back();
-    }
-
-    env->canvas->draw(name, (112/W) * env->canvas->w(), (90/H) * env->canvas->h(), color);
-
-    font->set_size(16);
-    env->canvas->draw(name, (250/W) * env->canvas->w(), (129/H) * env->canvas->h(), color);
-    font->set_size(18);
-    env->canvas->draw(military, (295/W) * env->canvas->w(), (167/H) * env->canvas->h(), Color(208, 179, 43));
-    env->canvas->draw(psionic, (295/W) * env->canvas->w(), (198/H) * env->canvas->h(), Color(166, 69, 151));
-    env->canvas->draw(tech, (295/W) * env->canvas->w(), (229/H) * env->canvas->h(), Color(78, 191, 190));
-
-    font->set_size(22);
-    env->canvas->draw(levelup, (500/W) * env->canvas->w(), (370/H) * env->canvas->h(), Color(62, 108, 236));
-
-    font->set_size(12);
-    env->canvas->draw(might, (607/W) * env->canvas->w(), (165/H) * env->canvas->h(), color);
-    env->canvas->draw(mind, (607/W) * env->canvas->w(), (200/H) * env->canvas->h(), color);
-    env->canvas->draw(perception, (607/W) * env->canvas->w(), (235/H) * env->canvas->h(), color);
-    env->canvas->draw(agility, (607/W) * env->canvas->w(), (270/H) * env->canvas->h(), color);
-
-    color = Color(82, 104, 93);
-    env->canvas->draw(might_attack, (780/W) * env->canvas->w(), (165/H) * env->canvas->h(), color);
-    env->canvas->draw(mind_attack, (780/W) * env->canvas->w(), (200/H) * env->canvas->h(), color);
-    env->canvas->draw(cooldown, (780/W) * env->canvas->w(), (235/H) * env->canvas->h(), color);
-    env->canvas->draw(defense, (780/W) * env->canvas->w(), (270/H) * env->canvas->h(), color);
-
-    env->canvas->draw(might_armor, (900/W) * env->canvas->w(), (165/H) * env->canvas->h(), color);
-    env->canvas->draw(mind_armor, (900/W) * env->canvas->w(), (200/H) * env->canvas->h(), color);
-    env->canvas->draw(critical, (900/W) * env->canvas->w(), (235/H) * env->canvas->h(), color);
-    env->canvas->draw(critical, (900/W) * env->canvas->w(), (270/H) * env->canvas->h(), color);
-
-    font->set_size(16);
-    auto settings = env->resources_manager->get_settings("res/datas/colony.sav");
-    int data = settings->read<int>("Colony", "data", 0);
-    env->canvas->draw(to_string(data), (500/W) * env->canvas->w(), (415/H) * env->canvas->h(), Color(170, 215, 190));
+    // font->set_size(16);
+    // auto settings = env->resources_manager->get_settings("res/datas/colony.sav");
+    // int data = settings->read<int>("Colony", "data", 0);
+    // env->canvas->draw(to_string(data), (500/W) * env->canvas->w(), (415/H) * env->canvas->h(), Color(170, 215, 190));
 }
 
 bool
@@ -271,4 +246,40 @@ Barracks::on_message(Object *sender, MessageID id, Parameters)
     }
 
     return true;
+}
+
+void
+Barracks::load_characters()
+{
+    Environment *env = Environment::get_instance();
+    
+    int x = (112 / W) * env->canvas->w();
+    int y = (130 / H) * env->canvas->h();
+    int w = (222 / W) * env->canvas->w();
+    int h = (270 / H) * env->canvas->h();
+
+    Character *character = new Character(this, "Isaac", "psionic_big.png", x, y, w, h, "Isaac");
+    m_characters[character->id()] = character;
+
+    // Character *character = new Character(this, "Albert", "psionic_big.png", x, y, w, h, "Albert");
+    // m_characters[character->id()] = character;
+
+    // Character *character = new Character(this, "Newton", "psionic_big.png", x, y, w, h, "Newton");
+    // m_characters[character->id()] = character;
+
+    // Character *character = new Character(this, "Clarke", "psionic_big.png", x, y, w, h, "Clarke");
+    // m_characters[character->id()] = character;
+
+    // Character *character = new Character(this, "Brooker", "psionic_big.png", x, y, w, h, "Brooker");
+    // m_characters[character->id()] = character;
+
+    // Character *character = new Character(this, "Michael", "psionic_big.png", x, y, w, h, "Michael");
+    m_characters[character->id()] = character;
+
+    for (auto it : m_characters)
+    {
+        it.second->add_observer(this);
+        add_child(it.second);
+        it.second->set_active(false);
+    }
 }
