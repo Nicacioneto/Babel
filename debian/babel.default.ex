f7c1e9fe38a60801c93f525069b10c22baf04b93
nicacioneto <nicacionetobsb@hotmail.com>
2017-05-28 19:41:43 -0300

    Adding files to deb package

---
+++ debian/babel.default.ex
@@ -0,0 +1,10 @@
+# Defaults for babel initscript
+# sourced by /etc/init.d/babel
+# installed at /etc/default/babel by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(babel):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="babel" command="/usr/bin/babel"
