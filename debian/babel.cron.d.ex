2ac6798e4fda3f057d9a03f7818c575f9e4e2b0b
nicacioneto <nicacionetobsb@hotmail.com>
2017-06-11 16:56:21 -0300

    Remove .ex files

--- debian/babel.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the babel package
-#
-0 4	* * *	root	[ -x /usr/bin/babel_maintenance ] && /usr/bin/babel_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(babel):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="babel" command="/usr/bin/babel"
